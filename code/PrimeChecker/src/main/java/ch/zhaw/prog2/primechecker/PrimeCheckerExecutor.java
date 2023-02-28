7d94712846de5f6e77efce65ea4b138b946fb9b0
an-Iceberg <a.g.marosi@bluewin.ch>
2023-02-28 21:49:40 +0100

    Formatted task #2 to my liking :3

--- code/PrimeChecker/src/main/java/ch/zhaw/prog2/primechecker/PrimeCheckerExecutor.java
+++ code/PrimeChecker/src/main/java/ch/zhaw/prog2/primechecker/PrimeCheckerExecutor.java
@@ -4,39 +4,49 @@ import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.TimeUnit;
 
-public class PrimeCheckerExecutor {
+public class PrimeCheckerExecutor
+{
+  private static final long LOWER_LIMIT = 10000L;
+  private static final long UPPER_LIMIT = 1000000000L;
+  private static final int NUM_PRIME = 500;
 
-    private static final long LOWER_LIMIT = 10000L;
-    private static final long UPPER_LIMIT = 1000000000L;
-    private static final int NUM_PRIME = 500;
+  public static void main(String[] args)
+  {
+    long starttime = System.currentTimeMillis();
+    long duration;
 
-    public static void main(String[] args) {
-        long starttime = System.currentTimeMillis();
-        long duration;
-        try {
-            checkPrimes(NUM_PRIME);
-        } catch (InterruptedException e) {
-            System.out.println("Interrupted - " + e.getMessage());
-        } finally {
-            duration = System.currentTimeMillis() - starttime;
-        }
-        System.out.println("Finished in " + duration + " ms");
+    try
+    {
+      checkPrimes(NUM_PRIME);
+    }
+    catch (InterruptedException e)
+    {
+      System.out.println("Interrupted - " + e.getMessage());
+    }
+    finally
+    {
+      duration = System.currentTimeMillis() - starttime;
     }
 
-    private static void checkPrimes(int numPrimes) throws InterruptedException {
-        // TODO: create ExecutorService - What ThreadPool-Type/-Size fits best?
+    System.out.println("Finished in " + duration + " ms");
+  }
 
-        for (int i = 0; i < numPrimes; i++) {
-            // TODO: execute the runnable using the executor service
+  private static void checkPrimes(int numPrimes) throws InterruptedException
+  {
+      // TODO: create ExecutorService - What ThreadPool-Type/-Size fits best?
 
-        }
-        // stop ExecutorService
+      for (int i = 0; i < numPrimes; i++)
+      {
+        // TODO: execute the runnable using the executor service
 
-        // wait for termination with timeout of 1 minute
+      }
+      // stop ExecutorService
 
-    }
+      // wait for termination with timeout of 1 minute
+  }
 
-    private static long nextRandom() {
-        return LOWER_LIMIT + (long)(Math.random() * (UPPER_LIMIT - LOWER_LIMIT));
-    }
+  private static long nextRandom()
+  {
+    return LOWER_LIMIT + (long)(Math.random() * (UPPER_LIMIT - LOWER_LIMIT));
+  }
 }
--- code/PrimeChecker/src/main/java/ch/zhaw/prog2/primechecker/PrimeTask.java
+++ code/PrimeChecker/src/main/java/ch/zhaw/prog2/primechecker/PrimeTask.java
@@ -1,32 +1,42 @@
 package ch.zhaw.prog2.primechecker;
 
-public class PrimeTask implements Runnable {
+public class PrimeTask implements Runnable
+{
+  private final long primeCandidate;
 
-    private final long primeCandidate;
+  public PrimeTask(long primeCandidate)
+  {
+    this.primeCandidate = primeCandidate;
+  }
 
-    public PrimeTask(long primeCandidate) {
-        this.primeCandidate = primeCandidate;
-    }
+  public void run()
+  {
+    long smallestFactor = findSmallestFactor(primeCandidate);
 
-    public void run() {
-        long smallestFactor = findSmallestFactor(primeCandidate);
-        System.out.println("Number: " + this.primeCandidate + "  -> " +
-            ( smallestFactor == 0 ? "PRIME" : "Factor: "  + smallestFactor ));
-    }
-    /**
-     * Brute force check if submitted candidate is a prime number.
-     *
-     * @param primeCandidate Number to check if it is a
-     * @return 0 if prime number, smallest factor otherwise
-     */
-    private long findSmallestFactor(long primeCandidate) {
-        if (primeCandidate>3) {
-            for(long factor = 2; factor <=primeCandidate/2; ++factor) {
-                if ( primeCandidate / factor * factor == primeCandidate)  {
-                    return factor; // found a factor -> is no prime
-                }
-            }
+    // TODO: use printf
+    System.out.println("Number: " + this.primeCandidate + "  -> " +
+      ( smallestFactor == 0 ? "PRIME" : "Factor: "  + smallestFactor ));
+  }
+
+  /**
+   * Brute force check if submitted candidate is a prime number.
+   *
+   * @param primeCandidate Number to check if it is a
+   * @return 0 if prime number, smallest factor otherwise
+   */
+  private long findSmallestFactor(long primeCandidate)
+  {
+    if (primeCandidate>3)
+    {
+      for(long factor = 2; factor <=primeCandidate/2; ++factor)
+      {
+        if ( primeCandidate / factor * factor == primeCandidate)
+        {
+          return factor; // found a factor -> is no prime
         }
-        return 0; // is prime number
+      }
     }
+
+    return 0; // is prime number
+  }
 }
